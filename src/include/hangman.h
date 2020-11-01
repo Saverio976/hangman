292405b8194a89b7482905da873ab38b9ce73515
Saverio <personnex976@gamil.com>
2020-11-01 18:44:51 +0100

    all function created add in header file

--- src/include/hangman.h
+++ src/include/hangman.h
@@ -16,10 +16,12 @@ typedef _Bool bool;
 #include "my_lib.h"
 
 //reinitialisation
-void reinitialiser_tab(char *mot_a_deviner, char *mot_afficher, char *input);
+void reinitialiser_tab(char *input, char *input_history);
 void reinitialiser_int(int *ptr_coup_restant);
 void set_mot_afficher(char *mot_afficher, int len_word);
-void set_up_game(char *mot_a_deviner, char *mot_afficher, char *input, int *ptr_coup_restant);
+void set_up_game1(char *secret_word, char *hidden_word, char *input, char *input_history, int *nb_joker);
+void set_up_game2(char *secret_word, char *hidden_word, char *input, char *input_history, int *nb_joker);
+void reinitialiser_secret_word(char *secret_word);
 
 
 //pick_word
@@ -30,10 +32,17 @@ int gen_random(int max);
 
 //tour_de_jeu
 int input_in_word(char *input, char *mot_a_deviner, char *mot_afficher);
-int is_end(char mot_afficher[]);
+bool is_end(char hidden_word[]);
 void check(char *input, char *secret_word, char *hidden_word, int *ptr_nb_joker);
-void show_information_in_game(int nb_joker, char *hidden_word);
+void show_information_in_game(int nb_joker, char *hidden_word, char *input_history);
 void show_information_end_game(char *secret_word, int nb_joker);
 bool is_replay();
+bool ft_is_one_player();
+
+//modif to db mot
+int db_to_upper();
+int add_word();
+int suppr_empty();
+int suppr_all_words();
 
 #endif
